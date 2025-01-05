84c38e42b8783ad895d610ad2808de32acfed04f
PabloA-Figueroa <apfigueroa3@utpl.edu.ec>
2025-01-04 21:45:18 -0500

    Add endpoints for retrieving personal maps and serving PDF files

--- app/adapters/home.py
+++ app/adapters/home.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+from typing import List
 from fastapi import  APIRouter, HTTPException
+from app.models.personal_map import ProfileData
 from data.database.mysql import MySqlConnection
+from fastapi.responses import FileResponse
+import os
 router = APIRouter()
 
 @router.get("/get/personal_maps")
@@ -14,3 +18,30 @@ async def get_personal_maps():
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/get/personal_map/completos", response_model=List[ProfileData])
+async def get_profile_data():
+    try:
+        db = MySqlConnection()
+        profile_data = db.get_all_personal_maps()
+        return profile_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+
+
+@router.get("/pdf", name="Obtener PDFs")
+async def get_file():
+    file_path = "/home/pablof/Documentos/study/data/pdfs/1._Plan_docente_test.pdf"
+    # Normalizar la ruta del archivo para diferentes sistemas operativos
+    normalized_file_path = os.path.normpath(file_path)
+    print("NORMAL",normalized_file_path)
+    full_file_path = os.path.join(os.getcwd(), normalized_file_path)
+    print("FULL",full_file_path)
+    # Verificar si el archivo existe
+    if not os.path.exists(full_file_path):
+        raise HTTPException(status_code=404, detail="Archivo no encontrado en el sistema de archivos")
+    # Retornar el archivo PDF
+    return FileResponse(path=full_file_path, filename=os.path.basename(full_file_path), media_type='application/pdf')
